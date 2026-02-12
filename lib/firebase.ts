ebfabba347f3fcd3c46e360f98a8362ef8137aed
unknown <lucasmeloil@pm.me>
2026-02-12 13:06:21 -0300

    atualiza banco de dados para maurobarber-44a91

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCABz2AFIgkyMy-ufqTlLwEOVGnt9RKR_c",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "mauro-b.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mauro-b",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "mauro-b.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "409690979779",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:409690979779:web:8583cfff175fc5951c1176",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-78ZK4KB8LF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyA6U7PrjbQlGtyAyTHT3Iik1LvgK50Kk_E",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "maurobarber-44a91.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "maurobarber-44a91",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "maurobarber-44a91.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "727174753035",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:727174753035:web:e2a5215db9135332fa100e",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-XXXXXXXXXX" // Adicione se necessário
 };
 
 // Initialize Firebase
