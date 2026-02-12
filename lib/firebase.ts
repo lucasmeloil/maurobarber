2e98759e9464a6476a552d9b5cd427fe789d4858
unknown <lucasmeloil@pm.me>
2026-02-12 12:10:46 -0300

    banco de dados

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCABz2AFIgkyMy-ufqTlLwEOVGnt9RKR_c",
-  authDomain: "mauro-b.firebaseapp.com",
-  projectId: "mauro-b",
-  storageBucket: "mauro-b.firebasestorage.app",
-  messagingSenderId: "409690979779",
-  appId: "1:409690979779:web:8583cfff175fc5951c1176"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCABz2AFIgkyMy-ufqTlLwEOVGnt9RKR_c",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "mauro-b.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mauro-b",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "mauro-b.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "409690979779",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:409690979779:web:8583cfff175fc5951c1176"
 };
 
 // Initialize Firebase
